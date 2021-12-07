970162a1b4d64c22f83b64a6f51303fdf266b43a
Anastasia Radchuk <71091432+ana-radchuk@users.noreply.github.com>
2021-12-07 14:04:29 +0200

    Update AkkaHttp.scala

--- src/main/scala/AkkaHttp.scala
+++ src/main/scala/AkkaHttp.scala
@@ -1,39 +1,39 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, HttpResponse}
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest}
 import akka.stream.ActorMaterializer
-
-import scala.concurrent.Future
+import akka.stream.scaladsl.Source
 import scala.concurrent.duration._
 
 object AkkaHttp {
 
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    import system.dispatcher
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
 
-    var req = HttpRequest(
-      method = HttpMethods.GET,
-      uri = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
-    )
 
-    val route: Route = (path("bpi") & get) {
-        sendRequest(req).foreach(println)
-        complete(sendRequest(req))
-    }
+  var req = HttpRequest(
+    method = HttpMethods.GET,
+    uri = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+  )
 
-    def sendRequest(request: HttpRequest): Future[String] = {
-      val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
-      val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(response =>
-        response.entity.toStrict(2.seconds))
-        entityFuture.map(entity => entity.data.utf8String)
-    }
+  val req2 = HttpRequest(
+    method = HttpMethods.GET,
+    uri = "https://api.coindesk.com/v1/bpi/currentprice.json"
+  )
+
+  def sendRequest(request: HttpRequest) = {
+    Source
+      .repeat(request)
+      .throttle(1, 30.seconds)
+      .mapAsync(1)(Http().singleRequest(_).flatMap(response =>
+        response.entity.toStrict(2.seconds)).map(entity => entity.data.utf8String))
+      .runForeach(println)
+  }
 
   def main(args: Array[String]): Unit = {
-    Http().newServerAt("localhost", 8080).bind(route)
-    println("Server is listening on port 8080")
+    sendRequest(req)
+    sendRequest(req2)
 
   }
 }
